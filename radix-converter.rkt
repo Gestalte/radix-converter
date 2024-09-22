ed521d67c1aa6072dbd9c32651f37c3daf095f9b
Gestalte <66849891+Gestalte@users.noreply.github.com>
2024-09-22 19:05:46 +0200

    Add error handling to octal input

--- radix-converter.rkt
+++ radix-converter.rkt
@@ -56,33 +56,48 @@
                                               (+ (calc (car lst) counter) (convert-radix-number-to-decimal (cdr lst) (+ counter 1) radix)))))
 
 (define decimal-changed (lambda (text-field event)
-                          ; TODO: If input contains [^0-9] show error
-                          (define input (string->number (send text-field get-value)))
-                          
-                          (define octal (~r input #:base 8))
-                          (define hex (~r input #:base '(up 16)))
-                          (define binary (~r input #:base 2))
-                          
-                          (send text-field-octal set-value octal)
-                          (send text-field-hex set-value hex)
-                          (send text-field-binary set-value binary)
-                          
-                          (print input)))
+                          (define show-error (lambda ()
+                                               (send text-field-octal set-value "Error")
+                                               (send text-field-hex set-value "Error")
+                                               (send text-field-binary set-value "Error")))
 
+                          (define set-values (lambda (num)
+                                               (define decimal-input (string->number num))
+                                               (define octal (~r decimal-input #:base 8))
+                                               (define hex (~r decimal-input #:base '(up 16)))
+                                               (define binary (~r decimal-input #:base 2))
+                                               (send text-field-octal set-value octal)
+                                               (send text-field-hex set-value hex)
+                                               (send text-field-binary set-value binary)))
+                          
+                          (define input (send text-field get-value))
+                          
+                          (if (regexp-match "[^0-9]" input)
+                              (show-error)
+                              (when (> (string-length input) 0)
+                                (set-values input)))))
+                              
 (define octal-changed (lambda (text-field event)
-                        ; TODO: If input contains [^0-9] show error
+                        (define show-error (lambda ()
+                                             (send text-field-decimal set-value "Error")
+                                             (send text-field-hex set-value "Error")
+                                             (send text-field-binary set-value "Error")))
+
+                        (define set-values (lambda (num)
+                                             (define input-list (reverse (string->list num)))
+                                             (define input-as-decimal (convert-radix-number-to-decimal input-list 0 8))
+                                             (send text-field-decimal set-value (number->string input-as-decimal))
+                                             (define hex (~r input-as-decimal #:base '(up 16)))
+                                             (define binary (~r input-as-decimal #:base 2))
+                                             (send text-field-binary set-value binary)                      
+                                             (send text-field-hex set-value hex)))
+                        
                         (define input (send text-field get-value))
-                        (define input-list (reverse (string->list input)))
-                        (define input-as-decimal (convert-radix-number-to-decimal input-list 0 8))
-
-                        (define hex (~r input-as-decimal #:base '(up 16)))
-                        (define binary (~r input-as-decimal #:base 2))
-
-                        (send text-field-decimal set-value (number->string input-as-decimal))
-                        (send text-field-binary set-value binary)                      
-                        (send text-field-hex set-value hex)
-                         
-                        (print input)))
+                          
+                        (if (regexp-match "[^0-8]" input)
+                            (show-error)
+                            (when (> (string-length input) 0)
+                              (set-values input)))))
 
 (define hex-changed (lambda (text-field event)
                       ; TODO: If input contains [^0-9A-F] show error
