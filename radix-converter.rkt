dce073f3603c76a47fd94d7fae59ee9fecef14ef
Gestalte <66849891+Gestalte@users.noreply.github.com>
2024-09-22 18:22:27 +0200

    All conversions now work.

--- radix-converter.rkt
+++ radix-converter.rkt
@@ -39,6 +39,22 @@
 
 ; Non-UI
 
+; lst should be the input number as a list in reverse.
+(define convert-radix-number-to-decimal (lambda (lst counter radix)
+                                          (define calc (lambda (item count)
+                                                         (cond
+                                                           [(eq? item #\A) (* 10 (expt radix count))]
+                                                           [(eq? item #\B) (* 11 (expt radix count))]
+                                                           [(eq? item #\C) (* 12 (expt radix count))]
+                                                           [(eq? item #\D) (* 13 (expt radix count))]
+                                                           [(eq? item #\E) (* 14 (expt radix count))]
+                                                           [(eq? item #\F) (* 15 (expt radix count))]
+                                                           [else (* (- (char->integer item) 48) (expt radix count))])))
+                                                       
+                                          (if (= (length lst) 1)
+                                              (calc (car lst) counter)
+                                              (+ (calc (car lst) counter) (convert-radix-number-to-decimal (cdr lst) (+ counter 1) radix)))))
+
 (define decimal-changed (lambda (text-field event)
                           (define input (string->number (send text-field get-value)))
                           (define octal (~r input #:base 8))
@@ -50,10 +66,44 @@
                           (print input)))
 
 (define octal-changed (lambda (text-field event)
-                        (print  (send text-field get-value))))
+                        (define input (send text-field get-value))
+                        (define input-list (reverse (string->list input)))
+                        (define input-as-decimal (convert-radix-number-to-decimal input-list 0 8))
+
+                        (define hex (~r input-as-decimal #:base '(up 16)))
+                        (define binary (~r input-as-decimal #:base 2))
+
+                        (send text-field-decimal set-value (number->string input-as-decimal))
+                        (send text-field-binary set-value binary)                      
+                        (send text-field-hex set-value hex)
+                         
+                        (print input)))
 
 (define hex-changed (lambda (text-field event)
-                      (print  (send text-field get-value))))
+                      (define input (send text-field get-value))
+                      (define input-list (reverse (string->list input)))
+                      (define input-as-decimal (convert-radix-number-to-decimal input-list 0 16))
+                      
+                      (define octal (~r input-as-decimal #:base 8))
+                      (define binary (~r input-as-decimal #:base 2))
+
+                      (send text-field-decimal set-value (number->string input-as-decimal))
+                      (send text-field-octal set-value octal)                      
+                      (send text-field-binary set-value binary)
+                      
+                      (print input)))
 
 (define binary-changed (lambda (text-field event)
-                         (print  (send text-field get-value))))
+                         (define input (send text-field get-value))
+                         (define input-list (reverse (string->list input)))
+                         (define input-as-decimal (convert-radix-number-to-decimal input-list 0 2))
+
+                         (define octal (~r input-as-decimal #:base 8))
+                         (define hex (~r input-as-decimal #:base '(up 16)))
+
+                         (send text-field-decimal set-value (number->string input-as-decimal))
+                         (send text-field-octal set-value octal)                      
+                         (send text-field-hex set-value hex)
+                         
+                         (print input)))
+
